e045340f95eb57d901fda80ace1336cb5a2317f6
Samuel Diop <snfdiop@outlook.com>
2020-05-25 12:43:29 -0700

    add cors

---
+++ app/CORS.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Set headers
+		w.Header().Set("Access-Control-Allow-Headers:", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		fmt.Println("ok")
+
+		// Next
+		next.ServeHTTP(w, r)
+		return
+	})
+}
