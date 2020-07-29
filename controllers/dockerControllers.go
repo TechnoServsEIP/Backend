9bdba0fc276dbaea7f8ae4c85847b318e74748eb
sullivan <sullivanverdan@live.fr>
2020-07-30 00:53:35 +0200

    test fix cors

--- controllers/dockerControllers.go
+++ controllers/dockerControllers.go
@@ -21,14 +21,14 @@ import (
 )
 
 var CreateDocker = func(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods",
-		"GET, POST, PATCH, PUT, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers",
-		"Accept, Content-Type, Content-Length, Accept-Encoding, "+
-			"X-CSRF-Token, Authorization, Origin, X-Auth-Token")
-	w.Header().Set("Access-Control-Expose-Headers",
-		"Authorization")
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	// w.Header().Set("Access-Control-Allow-Methods",
+	// 	"GET, POST, PATCH, PUT, DELETE, OPTIONS")
+	// w.Header().Set("Access-Control-Allow-Headers",
+	// 	"Accept, Content-Type, Content-Length, Accept-Encoding, "+
+	// 		"X-CSRF-Token, Authorization, Origin, X-Auth-Token")
+	// w.Header().Set("Access-Control-Expose-Headers",
+	// 	"Authorization")
 	// user := r.Context().Value("user").(uint) //Grab the id of the user that send the request
 	// fmt.Println("user: (", user, ")")
 
@@ -107,14 +107,14 @@ var CreateDocker = func(w http.ResponseWriter, r *http.Request) {
 }
 
 var StartDocker = func(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods",
-		"GET, POST, PATCH, PUT, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers",
-		"Accept, Content-Type, Content-Length, Accept-Encoding, "+
-			"X-CSRF-Token, Authorization, Origin, X-Auth-Token")
-	w.Header().Set("Access-Control-Expose-Headers",
-		"Authorization")
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	// w.Header().Set("Access-Control-Allow-Methods",
+	// 	"GET, POST, PATCH, PUT, DELETE, OPTIONS")
+	// w.Header().Set("Access-Control-Allow-Headers",
+	// 	"Accept, Content-Type, Content-Length, Accept-Encoding, "+
+	// 		"X-CSRF-Token, Authorization, Origin, X-Auth-Token")
+	// w.Header().Set("Access-Control-Expose-Headers",
+	// 	"Authorization")
 	ctx := context.Background()
 	userId := r.Context().Value("user").(uint) //Grab the id of the user that send the request
 	fmt.Println("userId: (", userId, ")")
@@ -164,14 +164,14 @@ var StartDocker = func(w http.ResponseWriter, r *http.Request) {
 }
 
 var StopDocker = func(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods",
-		"GET, POST, PATCH, PUT, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers",
-		"Accept, Content-Type, Content-Length, Accept-Encoding, "+
-			"X-CSRF-Token, Authorization, Origin, X-Auth-Token")
-	w.Header().Set("Access-Control-Expose-Headers",
-		"Authorization")
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	// w.Header().Set("Access-Control-Allow-Methods",
+	// 	"GET, POST, PATCH, PUT, DELETE, OPTIONS")
+	// w.Header().Set("Access-Control-Allow-Headers",
+	// 	"Accept, Content-Type, Content-Length, Accept-Encoding, "+
+	// 		"X-CSRF-Token, Authorization, Origin, X-Auth-Token")
+	// w.Header().Set("Access-Control-Expose-Headers",
+	// 	"Authorization")
 	ctx := context.Background()
 	userId := r.Context().Value("user").(uint) //Grab the id of the user that send the request
 	fmt.Println("userId: (", userId, ")")
