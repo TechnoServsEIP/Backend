a91a977fbbaf0771d18960cfbcd388b5d84c1862
sullivan <sullivanverdan@live.fr>
2020-08-23 21:43:19 +0200

    adding minecraft and utils test

---
+++ models/minecraft_test.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"testing"
+	"os/exec"
+)
+
+func TestkebabToCamelCase(t *testing.T) {
+	kebabString := []string{
+		"generate-structures",
+		"online-mode",
+		"max-build-height",
+		"level-seed",
+		"use-native-transport",
+		"prevent-proxy-connections"
+		"enable-jmx-monitoring",
+	}
+
+	if (kebabToCamelCase(kebabString[0]) != "GenerateStructures") {
+		t.Errorf("Error for string GenerateStructures")
+	}
+
+	if (kebabToCamelCase(kebabString[1]) != "OnlineMode") {
+		t.Errorf("Error for string OnlineMode")
+	}
+
+	if (kebabToCamelCase(kebabString[2]) != "MaxBuildHeight") {
+		t.Errorf("Error for string MaxBuildHeight")
+	}
+
+	if (kebabToCamelCase(kebabString[3]) != "LevelSeed") {
+		t.Errorf("Error for string LevelSeed")
+	}
+
+	if (kebabToCamelCase(kebabString[4]) != "UseNativeTransport") {
+		t.Errorf("Error for string UseNativeTransport")
+	}
+
+	if (kebabToCamelCase(kebabString[5]) != "PreventProxyConnections") {
+		t.Errorf("Error for string PreventProxyConnections")
+	}
+
+	if (kebabToCamelCase(kebabString[6]) != "EnableJmxMonitoring") {
+		t.Errorf("Error for string EnableJmxMonitoring")
+	}
+}
---
+++ utils/utils_test.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"testing"
+	"os/exec"
+)
+
+func TestCreateTmpFolder(t *testing.T) {
+	folderName := "testUtils"
+
+	if err := CreateTmpFolder(folderName); err != nil {
+		t.Errorf("Error when creating folder")
+	}
+
+	cmd := exec.Command("ls", folderName)
+
+	_, err := cmd.CombinedOutput()
+	
+	if err != nil {
+		t.Errorf("Error when test to ls folder")
+	}
+}
+
+func TestDeleteTmpFolder(t *testing.T) {
+	folderName := "testUtils"
+
+	if err := DeleteTmpFolder(folderName); err != nil {
+		t.Errorf("Error when deleting folder")
+	}
+
+	cmd := exec.Command("ls", folderName)
+
+	_, err := cmd.CombinedOutput()
+	
+	if err == nil {
+		t.Errorf("Error when test to ls folder")
+	}
+}
