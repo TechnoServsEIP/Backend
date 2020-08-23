78f80f4714943291b814cf9fa6a1ac1b4fb2a9d6
sullivan <sullivanverdan@live.fr>
2020-08-23 20:40:45 +0200

    adding route to update minecraft server.properties file

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func main() {
 	router.HandleFunc("/docker/infos", controllers.GetInfosUserServer).Methods("POST")
 	router.HandleFunc("/docker/playersonline", controllers.GetPlayersOnline).Methods("GET")
 	router.HandleFunc("/minecraft/serverproperties", controllers.GetServerProperties).Methods("GET")
+	router.HandleFunc("/minecraft/serverproperties", controllers.UpdateServerProperties).Methods("POST")
 	router.HandleFunc("/docker/update", controllers.ModifyGameServer).Methods("POST")
 	router.HandleFunc("/offers/list", controllers.ListOffers).Methods("GET")
 	router.HandleFunc("/offers/", controllers.GetOffer).Methods("POST")
--- models/minecraft.go
+++ models/minecraft.go
@@ -7,6 +7,7 @@ import (
     "encoding/json"
     "io/ioutil"
     "unicode"
+    "strconv"
     "strings"
     "reflect"
 
@@ -55,6 +56,42 @@ type ServerProperties struct {
     OnlineMode               properties `json:"OnlineMode"`
     LevelSeed                properties `json:"LevelSeed"`
     Motd                     properties `json:"Motd"`
+}
+
+type UpdateServerProperties struct {
+    UserId                  string  `json:"user_id"`
+    ContainerId             string  `json:"container_id"`
+
+	SpawnProtection          int    `json:"SpawnProtection"`
+    GeneratorSettings        string `json:"GeneratorSettings"`
+    ForceGamemode            bool   `json:"ForceGamemode"`
+    AllowNether              bool   `json:"AllowNether"`
+    EnforceWhitelist         bool   `json:"EnforceWhitelist"`
+    Gamemode                 string `json:"Gamemode"`
+    PlayerIdleTimeout        int    `json:"PlayerIdleTimeout"`
+    Difficulty               string `json:"Difficulty"`
+    SpawnMonsters            bool   `json:"SpawnMonsters"`
+    OpPermissionLevel        int    `json:"OpPermissionLevel"`
+    Pvp                      bool   `json:"Pvp"`
+    SnooperEnabled           bool   `json:"SnooperEnabled"`
+    LevelType                string `json:"LevelType"`
+    Hardcore                 bool   `json:"Hardcore"`
+    EnableStatus             bool   `json:"EnableStatus"`
+    EnableCommandBlock       bool   `json:"EnableCommandBlock"`
+    MaxPlayers               int    `json:"MaxPlayers"`
+    MaxWorldSize             int    `json:"MaxWorldSize"`
+    FunctionPermissionLevel  int    `json:"FunctionPermissionLevel"`
+    SpawnNpcs                bool   `json:"SpawnNpcs"`
+    AllowFlight              bool   `json:"AllowFlight"`
+    LevelName                string `json:"LevelName"`
+    ViewDistance             int    `json:"ViewDistance"`
+    ResourcePack             string `json:"ResourcePack"`
+    SpawnAnimals             bool   `json:"SpawnAnimals"`
+    WhitLlist                bool   `json:"WhitLlist"`
+    GenerateStructures       bool   `json:"GenerateStructures"`
+    OnlineMode               bool   `json:"OnlineMode"`
+    LevelSeed                string `json:"LevelSeed"`
+    Motd                     string `json:"Motd"`
 }
 
 func kebabToCamelCase(str string) string {
@@ -140,4 +177,127 @@ func ServerPropertiesByServerId(containerId string) *ServerProperties {
     }
 
 	return &data
+}
+
+/*
+ * Attach defaults and new values ​​from server.properties file
+ *
+ * data: new value send by the client
+ * tmpFolder: folder where will the server.properties
+ * containerId: the containerId of the client server
+ * serverFile: the path where the server.properties is into the client server
+ *
+ * return: map with each line of the server.properties file updating
+ */
+func getValueToUpdate(data UpdateServerProperties, tmpFolder string, containerId string, serverFile string) map[int]interface{} {
+    // create tmp folder
+    if err := utils.CreateTmpFolder(tmpFolder); err != nil {
+        fmt.Println(err)
+		return map[int]interface{}{}
+    }
+
+    // copy server.properties from the client container into tmpfolder
+	if err := utils.DockerCopy(serverFile, tmpFolder); err != nil {
+        fmt.Println(err)
+        
+        if err := utils.DeleteTmpFolder(tmpFolder); err != nil {
+            fmt.Println(err)
+            return map[int]interface{}{}
+        }
+
+		return map[int]interface{}{}
+    }
+
+    // read the server.properties and attach defaults and new values
+    fileServerProp, err := os.Open(tmpFolder + "/server.properties")
+    if err != nil {
+        fmt.Println(err)
+        return map[int]interface{}{}
+    }
+    defer fileServerProp.Close()
+
+    lines := make(map[int]interface{})
+    i := 0
+
+    scanner := bufio.NewScanner(fileServerProp)
+    for scanner.Scan() {
+        property := strings.Split(scanner.Text(), "=")
+
+        if (len(property) > 1) {
+            if (reflect.ValueOf(&data).Elem().FieldByName(kebabToCamelCase(property[0])) != reflect.Value{}) {
+                switch v := reflect.ValueOf(&data).Elem().FieldByName(kebabToCamelCase(property[0])); v.Kind() {
+                case reflect.Bool:
+                    lines[i] = property[0] + "=" + strconv.FormatBool(v.Bool())
+                case reflect.String:
+                    lines[i] = property[0] + "=" + v.String()
+                case reflect.Int:
+                    lines[i] = property[0] + "=" + strconv.FormatInt(v.Int(), 10)
+                }
+            } else {
+                lines[i] = property[0] + "=" + property[1]
+            }
+        }
+        i++
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Println(err)
+        return map[int]interface{}{}
+    }
+
+    return lines
+}
+
+/*
+ * Re create a new server.properties file with the new value sended by the client
+ *
+ * data: new value send by the client
+ * containerId: the containerId of the client server
+ *
+ * return: error or nil if no error 
+ */
+func CreateNewServerProperties(data UpdateServerProperties, containerId string) error {
+    tmpFolder := "./models/" + containerId
+    serverFile := containerId + ":/data/server.properties"
+    values := getValueToUpdate(data, tmpFolder, containerId, serverFile)
+
+    f, err := os.Create(tmpFolder + "/server.properties")
+    if err != nil {
+        fmt.Println(err)
+        f.Close()
+        return err
+    }
+
+    for _, v := range values {
+        fmt.Fprintln(f, v)
+        if err != nil {
+            fmt.Println(err)
+            return err
+        }
+    }
+    err = f.Close()
+    if err != nil {
+        fmt.Println(err)
+        return err
+    }
+
+    // Copy the new server.properties file into the client container
+    if err := utils.DockerCopy(tmpFolder + "/server.properties", containerId + ":/data"); err != nil {
+        fmt.Println(err)
+        
+        if err := utils.DeleteTmpFolder(tmpFolder); err != nil {
+            fmt.Println(err)
+            return err
+        }
+
+		return err
+    }
+
+    // remove the tmp folder
+    if err := utils.DeleteTmpFolder(tmpFolder); err != nil {
+        fmt.Println(err)
+        return err
+    }
+
+    return nil
 }
